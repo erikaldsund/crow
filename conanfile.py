b08e7166710cf779ab7f5f34668bae8262c670cc
erik <erik.sund@conoptica.com>
2020-08-30 12:49:54 +0200

    Set url to my repository
    ipkn repos has not been updated for three years and my pull requests have not
    been handled.

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ class CrowConan(ConanFile):
     name = "crow"
     version = "0.1"
     description = "Fast and easy to use modern C++ micro web framework"
-    url = "https://github.com/ipkn/crow"
-    license = "MIT; see https://github.com/ipkn/crow/blob/master/LICENSE"
+    url = "https://github.com/erikaldsund/crow"
+    license = "MIT; see https://github.com/erikaldsund/crow/blob/master/LICENSE"
     generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
 
